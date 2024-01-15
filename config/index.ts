477566ed55e30d0565783d7f0e7618169131cebf
MathieuTrachino <82705419+Matraque@users.noreply.github.com>
2024-01-15 17:36:18 +0000

    change logo, index, favicon

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'zh-Hans',
+  title: 'Dien Chan AI',
+  description: 'La description ira ici',
+  copyright: 'GenDojo.ai',
+  privacy_policy: 'la politique de confidentialité ira ici',
+  default_language: 'en',
 }
 
 export const isShowPrompt = false
